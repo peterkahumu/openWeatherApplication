0fdb2361adbcc656d3529706b47e835482d4e631
peterkahumu <muhumukip@gmail.com>
2024-09-13 15:20:41 +0300

    Working solution

--- weatherApplication/views.py
+++ weatherApplication/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if request.method == 'POST':
+        city = request.POST['city']
+        if city:
+            try:
+                source = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=adb1441f0b4802fd9396db393eabdd8d').read()
+                list_of_data = json.loads(source)
+
+                data = {
+                "country_code": str(list_of_data['sys']['country']),
+                "coordinate": str(list_of_data['coord']['lon']) + ', '
+                + str(list_of_data['coord']['lat']),
+
+                "temp": str(list_of_data['main']['temp']) + ' °C',
+                "pressure": str(list_of_data['main']['pressure']),
+                "humidity": str(list_of_data['main']['humidity']),
+                'main': str(list_of_data['weather'][0]['main']),
+                'description': str(list_of_data['weather'][0]['description']),
+                'icon': list_of_data['weather'][0]['icon'],
+                }
+                print(data)
+            except Exception as e:
+                data = {"error": "City not found!"}     
+    else:
+        data = {}
+
+    return render(request, "index.html", data)
