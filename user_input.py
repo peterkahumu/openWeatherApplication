3ba696dda29bc172010675e7b7f91c51ac5219b5
peterkahumu <muhumukip@gmail.com>
2024-09-13 08:43:59 +0300

    initial commit

--- user_input.py
+++
@@ -1,5 +0,0 @@
-name = input("Please Enter your Name: ")
-age = input("Please Enter your Age: ")
-location = input("Please Enter your Current Location: ")
-
-print(f"Hello {name}, you are {age} years old and you live in {location}")
